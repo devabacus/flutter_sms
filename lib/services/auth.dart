c7d7b97c95268a25c47cab545bc365fdec70ca61
devabacusnrf <devabacusnrf@gmail.com>
2019-10-07 06:22:32 +0500

    Streams

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -29,6 +29,10 @@ class Auth implements AuthBase{
     return User(uid: user.uid);
   }
 
+  Stream<User> get onAuthStateChanged{
+     return _firebaseAuth.onAuthStateChanged.map(_userFromFirebase);
+  }
+
   Future<User> currentUser() async {
     FirebaseUser user =  await _firebaseAuth.currentUser();
     return _userFromFirebase(user);
